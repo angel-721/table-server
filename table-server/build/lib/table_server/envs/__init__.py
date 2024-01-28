6d7236cc64bcb4bf7bfcec02cd6ca88d5799a118
angel-721 <angelvelasquez2002@gmail.com>
2024-01-28 12:01:09 -0700

    clear old stuff

--- table-server/build/lib/table_server/envs/__init__.py
+++
@@ -1,3 +0,0 @@
-from table_server.envs.table_server_env import TableServerEnv
-from table_server.envs.table_server_model import TableServerModel
-from table_server.envs.table_server_model import TableServerState
--- table-server/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="table_server",
-    version="0.0.1",
-    install_requires=["gym>=0.26.0", "pygame>=2.1.0"],
-)
